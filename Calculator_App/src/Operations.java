7a1b1ea299dc289e49bb00b88ad07bb1030f29d5
Akanksha Mate <85989430+akankshamate@users.noreply.github.com>
2023-04-18 05:42:37 -0400

    Add files via upload

---
+++ Calculator_App/src/Operations.java
@@ -0,0 +1,21 @@
+public class Operations {
+    public void add(double a,double b){
+        System.out.println("Addition of "+a+" and "+b+" is "+(a+b));
+    }
+    public void sub(double a,double b){
+        System.out.println("Subtraction of "+a+" and "+b+" is "+(a-b));
+    }
+    public void mul(double a,double b){
+        System.out.println("Multiplication of "+a+" and "+b+" is "+(a*b));
+    }
+    public void div(double a,double b){
+        System.out.println("Division of "+a+" and "+b+" is "+(a/b));
+    }
+
+    public void square(double a){
+        System.out.println("Square of "+a+" is "+(a*a));
+    }
+    public void cube(double a){
+        System.out.println("Cube of "+a+" is "+(a*a*a));
+    }
+}
